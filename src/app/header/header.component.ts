cd577e6f66489df66edf0f46886122ff3f7215dc
sbasti <slim.basti@spectrumgroupe.fr>
2018-02-09 10:54:36 +0100

    Creating the Components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by spectrum on 09/02/2018.
+ */
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
